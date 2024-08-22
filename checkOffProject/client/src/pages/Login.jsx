0a6f67c1849a6ce2e089a5af323d4965fa72a7da
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2024-08-22 03:12:59 -0700

    add login.jsx

---
+++ checkOffProject/client/src/pages/Login.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser } from '../redux/actions/authActions';
+
+const Login = () => {
+    const [formState, setFormState] = useState({ email: '', password: ''});
+    const dispatch = useDispatch();
+    const { loading, error } = useSelector((state) => state.auth);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = (event) => { 
+        event.preventDefault();
+        dispatch(loginUser(formState));
+    };
+
+    return (
+        <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-lg-10">
+        <div className="card">
+          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+          <div className="card-body">
+            {loading ? (
+              <p>Loading...</p>
+            ) : (
+              <form onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <button
+                  className="btn btn-block btn-info"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
+
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+
+};
+
+export default Login;
