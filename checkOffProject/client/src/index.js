8fc90bd2f020b50970d9c3a04f5745af7c35e2a0
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2024-10-03 19:07:04 -0700

    move files

--- checkOffProject/client/src/index.js
+++ checkOffProject/client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store.js';
+import store from './redux/store/store.js';
 import App from './App.jsx';
 
 ReactDOM.render(
