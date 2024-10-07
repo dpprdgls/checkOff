aed7ffc5cd734aef64d554573826a3bb02dad8c3
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2024-10-07 12:29:09 -0700

    fix typo

--- checkOffProject/server/routes/apiRoutes/taskRoutes.js
+++ checkOffProject/server/routes/apiRoutes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const taskController = require('../../controllers/taskControlle.js');
+const taskController = require('../../controllers/taskController.js');
 
 router.get('/', taskController.getAllTasks);
 
