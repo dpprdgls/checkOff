f89f160fb6da797d27406f8b8aa79473edf96778
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2024-10-24 11:13:13 -0700

    add tailwind

--- checkOffProject/client/src/App.jsx
+++ checkOffProject/client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './styles/style.css';
+import './styles/tailwind.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
--- checkOffProject/client/src/index.js
+++ checkOffProject/client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'; // Updated import for React 18
 import { Provider } from 'react-redux';
 import store from './redux/store/store.js';
 import App from './App.jsx';
+import './styles/tailwind.css';
 
 // Create a root and render the app
 const root = ReactDOM.createRoot(document.getElementById('root'));
