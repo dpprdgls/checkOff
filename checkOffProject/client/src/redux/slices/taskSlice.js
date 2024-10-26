bd35dc58ff5684441301c278a33003816680aa2d
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2024-10-26 12:59:54 -0700

    clean up

--- checkOffProject/client/src/redux/slices/taskSlice.js
+++ checkOffProject/client/src/redux/slices/taskSlice.js
@@ -1,26 +1,3 @@
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchTasks } from './path/to/taskSlice'; // Adjust path to taskSlice
-
-// const TaskList = () => {
-//   const dispatch = useDispatch();
-//   const tasks = useSelector((state) => state.tasks.tasks);
-
-//   useEffect(() => {
-//     const userId = localStorage.getItem('userId'); // Assuming user ID is stored after login
-//     dispatch(fetchTasks(userId)); // Pass the user ID to fetch tasks
-//   }, [dispatch]);
-
-//   return (
-//     <div>
-//       {tasks.map((task) => (
-//         <TaskCard key={task._id} task={task} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default TaskList;
 
 
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
--- checkOffProject/client/src/redux/store/store.js
+++ checkOffProject/client/src/redux/store/store.js
@@ -1,22 +1,11 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import rootReducer from '../reducers/index.js';
-
-// const store = configureStore({
-//   reducer: rootReducer,
-//   devTools: process.env.NODE_ENV !== 'production', // Enable Redux DevTools in development mode
-// });
-
-// export default store;
 
 // redux/store.js
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import taskReducer from '../reducers/taskReducer.js'; // Assuming this is the path to your task reducer
-// import userReducer from '../reducers/userReducer.js'; // Import other reducers here
 import authReducer from '../reducers/authReducer.js'; // Assuming this is the path to your auth reducer
 const rootReducer = combineReducers({
   tasks: taskReducer,
   auth: authReducer,
-  // user: userReducer, // Add your user reducer or other reducers here
   // other reducers...
 });
 
