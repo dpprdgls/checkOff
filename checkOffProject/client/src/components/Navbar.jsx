35655589e26b72a3d0ec1493a5c9cdd58bd854a3
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2024-11-14 23:46:52 -0800

    remove old style folder and file

--- checkOffProject/client/src/components/Navbar.jsx
+++ checkOffProject/client/src/components/Navbar.jsx
@@ -1,35 +1,49 @@
-import React, { useState } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
+
+
 import '../styles/tailwind.css';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
 
 const NavBar = () => {
-    const [nav, setNav] = useState(false);
-    const location = useLocation();
-    // const handleClick = () => setNav(!nav);
+  const [nav, setNav] = useState(false);
 
-
-
-return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-500 text-gray-300 mb-8'>
-         <ul className='hidden md:flex'>
-            <li>
-            <NavLink to='/' className={location.pathname === '/' ? 'active-link' : ''}>
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-800 text-gray-300 mb-8'>
+      <ul className='hidden md:flex'>
+        <li>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              isActive ? 'text-orange-500 px-4 cursor-pointer' : 'px-4 cursor-pointer'
+            }
+          >
             Home
           </NavLink>
-            </li>
-            <li>
-            <NavLink to='/login/:id/tasks' className={location.pathname === '/tasks' ? 'active-link' : ''}>
+        </li>
+        <li>
+          <NavLink
+            to='/login/:id/tasks'
+            className={({ isActive }) =>
+              isActive ? 'text-orange-500 px-4 cursor-pointer' : 'px-4 cursor-pointer'
+            }
+          >
             Tasks
           </NavLink>
-            </li>
-            <li>
-            <NavLink to='/projects' className={location.pathname === '/projects' ? 'active-link' : ''}>
+        </li>
+        <li>
+          <NavLink
+            to='/projects'
+            className={({ isActive }) =>
+              isActive ? 'text-orange-500 px-4 cursor-pointer' : 'px-4 cursor-pointer'
+            }
+          >
             Projects
           </NavLink>
-            </li>
-         </ul>
+        </li>
+      </ul>
     </div>
-);
-
+  );
 };
+
 export default NavBar;
