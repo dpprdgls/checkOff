157022df14ff56b46c24e1c8b3636b64c4ae3c85
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2024-11-21 19:38:36 -0800

    remove old code

--- checkOffProject/client/src/App.jsx
+++ checkOffProject/client/src/App.jsx
@@ -1,38 +1,4 @@
-// import React from 'react';
-// import './styles/tailwind.css';
-// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-// import LoginForm from './components/LoginForm';
-// import RegisterForm from './components/RegisterForm';
-// import Home from './components/Home';
-// import Tasks from './components/Tasks'; 
-// import NavBar from './components/Navbar';
-// import Projects from './components/Projects';
 
-// const App = () => {
-//   return (
-//     <Router>
-//       {/* Main App Container */}
-//       <div className="bg-white text-stone-900 min-h-screen font-inter">
-//         {/* NavBar at the top */}
-//         <NavBar />
-
-//         {/* Page Content Area */}
-//         <div className="max-w-screen-lg mx-auto mt-8 p-4"> {/* Centers content and adds padding */}
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/login" element={<LoginForm />} />
-//             <Route path="/register" element={<RegisterForm />} />
-//             <Route path="/login/:id/tasks" element={<Tasks />} />
-//             <Route path="/projects" element={<Projects />} />
-//           </Routes>
-//         </div>
-//       </div>
-//     </Router>
-//   );
-// };
-
-
-// export default App;
 // App.jsx
 import React from 'react';
 import './styles/tailwind.css';
